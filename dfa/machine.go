c975c8ec72b3136d0bde90fef408bfbc7529aed1
Andy Cook <andrewcook1990@gmail.com>
2017-04-17 15:41:36 +0100

    More tests for state machine and more explicit errors

--- dfa/machine.go
+++ dfa/machine.go
@@ -18,12 +18,21 @@ type Machine interface {
 }
 
 type machine struct {
-	current *state
-	states  map[string]*state
+	current  *state
+	states   map[string]*state
+	finished bool
 }
 
+var MachineUnusable = errors.New("Machine is in an unusable state. Has it already finished?")
+
+type UnacceptableMachineFinishState error
+
+type UnknownMachineState error
+
+type InvalidMachineTransition error
+
 func newMachine() *machine {
-	return &machine{states: make(map[string]*state)}
+	return &machine{states: make(map[string]*state), finished: false}
 }
 
 func newState(name string) *state {
@@ -36,26 +45,40 @@ func newState(name string) *state {
 }
 
 func (machine *machine) Transition(how string) error {
+	if machine.finished {
+		return MachineUnusable
+	}
+
 	if _, exists := machine.current.paths[how]; !exists {
 		return errors.New(fmt.Sprintf("Don't know how to move from %s via path %s", machine.current.name, how))
 	}
 
 	machine.current = machine.current.paths[how]
 
+	// Call all functions as we enter this new state
+	for _, fn := range machine.current.whenEntering {
+		fn()
+	}
+
 	return nil
 }
 
 func (machine *machine) Finish() error {
+	if machine.finished {
+		return MachineUnusable
+	}
+
 	if machine.current.acceptable {
+		machine.finished = true
 		return nil
 	}
 
-	return errors.New(fmt.Sprintf("Cannot accept final state %s", machine.current.name))
+	return UnacceptableMachineFinishState(errors.New(fmt.Sprintf("Unacceptable finish state: %s", machine.current.name)))
 }
 
 func validateState(machine *machine, state string) error {
 	if _, exists := machine.states[state]; !exists {
-		return errors.New(fmt.Sprintf("Unknown state %s", state))
+		return UnknownMachineState(errors.New(fmt.Sprintf("Unknown machine state: %s", state)))
 	}
 
 	return nil
--- dfa/machine_test.go
+++ dfa/machine_test.go
@@ -34,16 +34,11 @@ func TestAcceptTwoState(t *testing.T) {
 	builder.Path("one", "via", "two")
 	builder.Accept("two")
 
-	machine, err := builder.Start("one")
-
-	if err != nil {
-		t.Fatalf("Unexpected error when starting machine: %v", err)
-	}
+	machine := build(builder, "one", t)
 
 	machine.Transition("via")
-	err = machine.Finish()
 
-	if err != nil {
+	if err := machine.Finish(); err != nil {
 		t.Errorf("Expected machine acceptance, but it failed: %v", err)
 	}
 }
@@ -53,16 +48,12 @@ func TestFailTwoState(t *testing.T) {
 
 	builder.Path("one", "via", "two")
 
-	machine, err := builder.Start("one")
-
-	if err != nil {
-		t.Fatalf("Unexpected error when starting machine: %v", err)
-	}
+	machine := build(builder, "one", t)
 
 	machine.Transition("via")
-	err = machine.Finish()
 
-	if err == nil {
+	err := machine.Finish()
+	if _, expected := err.(dfa.UnacceptableMachineFinishState); !expected {
 		t.Error("Expected machine to fail, but it accepted")
 	}
 }
@@ -80,17 +71,109 @@ func TestInvalidTransition(t *testing.T) {
 
 	builder.Path("one", "via", "two")
 
-	machine, err := builder.Start("one")
+	machine := build(builder, "one", t)
+
+	if err := machine.Transition("not-via"); err == nil {
+		t.Error("Expected Transition() to fail on non-existent path")
+	}
+}
+
+func TestWhenEnteringIsCalled(t *testing.T) {
+	n := 0
+
+	inc := func() {
+		n++
+	}
+
+	builder := getMachineBuilder()
+	builder.Path("from", "via", "to")
+	builder.WhenEntering("to", inc)
+	builder.WhenEntering("to", inc)
+	builder.WhenEntering("to", inc)
+
+	machine := build(builder, "from", t)
+
+	machine.Transition("via")
+
+	if n != 3 {
+		t.Errorf("Expected WhenEntering() to invoke functions, but it didn't. n: %d", n)
+	}
+}
+
+func TestMachineTrace(t *testing.T) {
+	trace := ""
+
+	getTraceFn := func(str string) func() {
+		return func() {
+			trace += " " + str
+		}
+	}
+
+	builder := getMachineBuilder()
+
+	builder.Path("one", "one-two", "two")
+	builder.Path("two", "two-three", "three")
+	builder.Path("three", "three-five", "five")
+	builder.Path("four", "four-five", "five")
+	builder.Path("five", "five-four", "four")
+	builder.Path("five", "five-one", "one")
+	builder.WhenEntering("one", getTraceFn("one"))
+	builder.WhenEntering("two", getTraceFn("two"))
+	builder.WhenEntering("three", getTraceFn("three"))
+	builder.WhenEntering("four", getTraceFn("four"))
+	builder.WhenEntering("five", getTraceFn("five"))
+	builder.Accept("one")
+
+	expected := " two three five four five one"
+
+	machine := build(builder, "one", t)
+
+	machine.Transition("one-two")
+	machine.Transition("two-three")
+	machine.Transition("three-five")
+	machine.Transition("five-four")
+	machine.Transition("four-five")
+	machine.Transition("five-one")
+
+	if err := machine.Finish(); err != nil {
+		t.Errorf("Expected machine to accept, but it didn't: %v", err)
+	}
+
+	if expected != trace {
+		t.Errorf("Expected trace '%s' but got '%s'", expected, trace)
+	}
+}
+
+func TestMachineCannotBeUsedAfterCompletion(t *testing.T) {
+	builder := getMachineBuilder()
+
+	builder.Path("origin", "moveme", "origin")
+
+	builder.Accept("origin")
+
+	machine := build(builder, "origin", t)
+
+	if err := machine.Finish(); err != nil {
+		t.Errorf("Expected machine to accept, but it didn't: %v", err)
+	}
+
+	if err := machine.Transition("moveme"); err == nil {
+		t.Error("Expected second Transition() to fail as machine unusable, but it didn't")
+	}
+
+	if err := machine.Finish(); err == nil {
+		t.Error("Expected second Finish() to fail as machine unusable, but it didn't")
+	}
+}
+
+func build(builder dfa.MachineBuilder, start string, t *testing.T) dfa.Machine {
+	machine, err := builder.Start(start)
 
 	if err != nil {
 		t.Fatalf("Unexpected error when starting machine: %v", err)
 	}
 
-	err = machine.Transition("not-via")
-
-	if err == nil {
-		t.Error("Expected Transition() to fail on non-existent path")
-	}
+	return machine
 }
 
 func getMachineBuilder() dfa.MachineBuilder {
