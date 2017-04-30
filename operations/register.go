7d48412c2d612d274403394efe2878e8df3e5c6a
Andy Cook <andrewcook1990@gmail.com>
2017-04-30 15:28:36 +0100

    Registry for operators, external to the parser.
    * Will be used by the parser to resolve operator precedence.
    * Hopefully we'll plug this in with evaluation/operation resolution (see comments around Operation interface).

---
+++ operations/register.go
@@ -0,0 +1,55 @@
+package operations
+
+import (
+	"fmt"
+)
+
+// Defines an operation (e.g. +, * etc)
+// For now, this simply defines precedence,
+// but we intend that implementations of this
+// can define the operation itself, and using
+// reflection we can resolve an operator
+// to a function that will actually perform the
+// operation. Reflection _may_ allow us to have
+// different signatures for the same operator,
+// (e.g. "foo" + "bar" vs 1 + 2), but this will
+// need some more thinking, especially around
+// any cross over with functions. To proceed here,
+// we need to flesh out the evaluator more and consider
+// how values are represented and passed to functions.
+type Operation interface {
+	Operator() string
+	Precedence() int
+}
+
+type Register struct {
+	operations map[string]Operation
+}
+
+type UnknownOperatorError struct {
+	operator string
+}
+
+func (err UnknownOperatorError) Error() string {
+	return fmt.Sprintf("Unknown operator: %s", err.operator)
+}
+
+func NewRegister() *Register {
+	return &Register{operations: make(map[string]Operation)}
+}
+
+func (r *Register) Register(operation Operation) error {
+	r.operations[operation.Operator()] = operation
+
+	return nil
+}
+
+func (r *Register) TakesPrecedence(what string, over string) (bool, error) {
+	if whatOperator, exists := r.operations[what]; !exists {
+		return false, UnknownOperatorError{operator: what}
+	} else if overOperator, exists := r.operations[over]; !exists {
+		return false, UnknownOperatorError{operator: over}
+	} else {
+		return whatOperator.Precedence() > overOperator.Precedence(), nil
+	}
+}
